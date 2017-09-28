d1a47d6e73c88dafd935888c8f0ded3e47170343
Oliver Lemke <olemke@core-dump.info>
2017-09-28 15:21:39 +0200

    Added skeleton for hitran xsec species.

--- src/workspace.cc
+++ src/workspace.cc
@@ -2058,6 +2058,15 @@ void Workspace::define_wsv_data()
       GROUP( "Agenda" )));
 
   wsv_data.push_back
+    (WsvRecord
+     ( NAME( "hitran_xsec_data" ),
+       DESCRIPTION
+       (
+        "Dummy variable for testing the HITRAN xsec module.\n"
+        ),
+      GROUP( "Matrix" )));
+
+  wsv_data.push_back
    (WsvRecord
     ( NAME( "instrument_pol" ),
       DESCRIPTION
